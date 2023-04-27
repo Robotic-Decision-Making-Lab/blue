8fbe2d2127477ce11171440aec4a531aba3a4c18
Evan Palmer <evanp922@gmail.com>
2023-04-27 16:00:15 -0700

    Started refactoring to resolve pr comments

--- blue_dynamics/include/blue_dynamics/hydrodynamics.hpp
+++ blue_dynamics/include/blue_dynamics/hydrodynamics.hpp
@@ -22,235 +22,72 @@
 
 #include <Eigen/Dense>
 
+#include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
 namespace blue::dynamics
 {
 
 /**
- * @brief A common base class to use for defining a collection of N-D hydrodynamics coefficients.
- */
-struct HydrodynamicsXd
-{
-  /**
-   * @brief Convert the coefficients into a column vector.
-   *
-   * @return The coefficients as a vector.
-   */
-  [[nodiscard]] virtual Eigen::VectorXd toVector() const = 0;
-
-  /**
-   * @brief Convert the coefficients into a matrix.
-   *
-   * @return The coefficients as a matrix.
-   */
-  [[nodiscard]] virtual Eigen::MatrixXd toMatrix() const = 0;
-};
-
-/**
- * @brief A base struct that can be used to define a collection of 6-D hydrodynamic coefficients.
- */
-struct Hydrodynamics6d : HydrodynamicsXd
-{
-  double x;  // The X coefficient.
-  double y;  // The Y coefficient.
-  double z;  // The Z coefficient.
-  double k;  // The K coefficient.
-  double m;  // The M coefficient.
-  double n;  // The Z coefficient.
-
-  /**
-   * @brief Construct a new Hydrodynamics6d object.
-   *
-   * @param x The X coefficient.
-   * @param y The Y coefficient.
-   * @param z The Z coefficient.
-   * @param k The K coefficient.
-   * @param m The M coefficient.
-   * @param n The Z coefficient.
-   */
-  Hydrodynamics6d(double x, double y, double z, double k, double m, double n);
-
-  /**
-   * @brief Create a vector using the coefficients.
-   *
-   * @return The coefficients as a column vector.
-   */
-  [[nodiscard]] Eigen::VectorXd toVector() const override;
-
-  /**
-   * @brief Create a matrix using the coefficients.
-   *
-   * @note The base struct creates a diagonal matrix from the coefficients.
-   *
-   * @return The coefficients as a 6x6 diagonal matrix.
-   */
-  [[nodiscard]] Eigen::MatrixXd toMatrix() const override;
-};
-
-/**
- * @brief A base struct that can be used to define a collection of 3-D hydrodynamic coefficients.
- */
-struct Hydrodynamics3d : HydrodynamicsXd
-{
-  double x;  // The X coefficient.
-  double y;  // The Y coefficient.
-  double z;  // The Z coefficient.
-
-  /**
-   * @brief Construct a new Hydrodynamics3d object
-   *
-   * @param x The X coefficient.
-   * @param y The Y coefficient.
-   * @param z The Z coefficient.
-   */
-  Hydrodynamics3d(double x, double y, double z);
-
-  /**
-   * @brief Create a vector from the coefficients.
-   *
-   * @return The coefficients as a column vector.
-   */
-  [[nodiscard]] Eigen::VectorXd toVector() const override;
-
-  /**
-   * @brief Create a matrix from the coefficients.
-   *
-   * @note The base struct creates a diagonal matrix from the coefficients.
-   *
-   * @return The coefficients as a 3x3 diagonal matrix.
-   */
-  [[nodiscard]] Eigen::MatrixXd toMatrix() const override;
-};
-
-/**
- * @brief The moments of inertia of a rigid body.
- */
-struct MomentsOfInertia : Hydrodynamics3d
-{
-  /**
-   * @brief Construct a new default MomentsOfInertia object.
-   */
-  MomentsOfInertia();
-
-  /**
-   * @brief Construct a new MomentsOfInertia object.
-   *
-   * @param x The I_xx coefficient.
-   * @param y The I_yy coefficient.
-   * @param z The I_zz coefficient.
-   */
-  MomentsOfInertia(double x, double y, double z);
-};
-
-/**
- * @brief The center of buoyancy of a submerged body.
- */
-struct CenterOfBuoyancy : Hydrodynamics3d
-{
-  /**
-   * @brief Construct a new default CenterOfBuoyancy object.
-   */
-  CenterOfBuoyancy();
-
-  /**
-   * @brief Construct a new CenterOfBuoyancy object.
-   *
-   * @param x The center of buoyancy along the x-axis.
-   * @param y The center of buoyancy along the y-axis.
-   * @param z The center of buoyancy along the z-axis.
-   */
-  CenterOfBuoyancy(double x, double y, double z);
-};
-
-/**
- * @brief The center of gravity of a submerged body.
- */
-struct CenterOfGravity : Hydrodynamics3d
-{
-  /**
-   * @brief Construct a new default CenterOfGravity object.
-   */
-  CenterOfGravity();
-
-  /**
-   * @brief Construct a new CenterOfGravity object.
-   *
-   * @param x The center of gravity along the x-axis.
-   * @param y The center of gravity along the y-axis.
-   * @param z The center of gravity along the z-axis.
-   */
-  CenterOfGravity(double x, double y, double z);
-};
-
-/**
- * @brief The added mass hydrodynamic coefficients.
- */
-struct AddedMass : Hydrodynamics6d
-{
-  /**
-   * @brief Construct a new default AddedMass object.
-   */
-  AddedMass();
-
-  /**
-   * @brief Construct a new AddedMass object.
-   *
-   * @param x_u_dot The X_dot{u} coefficient.
-   * @param y_v_dot The X_dot{v} coefficient.
-   * @param z_w_dot The Z_dot{w} coefficient.
-   * @param k_p_dot The K_dot{p} coefficient.
-   * @param m_q_dot The K_dot{q} coefficient.
-   * @param n_r_dot The N_dot{r} coefficient.
-   */
-  AddedMass(
-    double x_u_dot, double y_v_dot, double z_w_dot, double k_p_dot, double m_q_dot, double n_r_dot);
-};
-
-/**
- * @brief Linear damping coefficients.
+ * @brief Create a skew-symmetric matrix from the provided coefficients.
  *
+ * @param a1 Coefficient one.
+ * @param a2 Coefficient two.
+ * @param a3 Coefficient three.
+ * @return The created skew-symmetric matrix.
  */
-struct LinearDamping : Hydrodynamics6d
-{
-  /**
-   * @brief Construct a new default LinearDamping object.
-   */
-  LinearDamping();
+[[nodiscard]] static Eigen::Matrix3d createSkewSymmetricMatrix(double a1, double a2, double a3);
 
-  /**
-   * @brief Construct a new LinearDamping object.
-   *
-   * @param x_u The X_u coefficient.
-   * @param y_v The Y_v coefficient.
-   * @param z_w The Z_w coefficient.
-   * @param k_p The K_p coefficient.
-   * @param m_q The M_q coefficient.
-   * @param n_r The N_r coefficient.
-   */
-  LinearDamping(double x_u, double y_v, double z_w, double k_p, double m_q, double n_r);
+class Intertia
+{
+public:
+  Intertia(double mass, const Eigen::Matrix3d & moments, const Eigen::MatrixXd & added_mass);
+
+  [[nodiscard]] Eigen::MatrixXd calculateIntertia() const;
+
+private:
+  Eigen::MatrixXd inertia_matrix_;
 };
 
-/**
- * @brief Nonlinear damping coefficients.
- *
- */
-struct NonlinearDamping : Hydrodynamics6d
+class Coriolis
 {
-  /**
-   * @brief Construct a new default NonlinearDamping object.
-   */
-  NonlinearDamping();
+public:
+  Coriolis(double mass, const Eigen::Matrix3d & moments, const Eigen::MatrixXd & added_mass);
 
-  /**
-   * @brief Construct a new NonlinearDamping object
-   *
-   * @param x_uu The X_{u|u|} coefficient.
-   * @param y_vv The Y_{v|v|} coefficient.
-   * @param z_ww The Z_{w|w|} coefficient.
-   * @param k_pp The K_{p|p|} coefficient.
-   * @param m_qq The M_{q|q|} coefficient.
-   * @param n_rr The N_{r|r|} coefficient.
-   */
-  NonlinearDamping(double x_uu, double y_vv, double z_ww, double k_pp, double m_qq, double n_rr);
+  [[nodiscard]] Eigen::MatrixXd calculateCoriolis(const Eigen::VectorXd & velocity) const;
+
+private:
+  double mass_;
+  Eigen::Matrix3d moments_;
+  Eigen::MatrixXd added_mass_;
+
+  [[nodiscard]] Eigen::MatrixXd calculateRigidBodyCoriolis(const Eigen::VectorXd & velocity) const;
+  [[nodiscard]] Eigen::MatrixXd calculateAddedCoriolis(const Eigen::VectorXd & velocity) const;
+};
+
+class Damping
+{
+public:
+  Damping(const Eigen::MatrixXd & linear_damping, const Eigen::MatrixXd & quadratic_damping);
+
+  [[nodiscard]] Eigen::MatrixXd calculateDamping(const geometry_msgs::msg::Twist & velocity) const;
+
+private:
+  Eigen::MatrixXd linear_damping_;
+  Eigen::MatrixXd quadratic_damping_;
+
+  [[nodiscard]] Eigen::MatrixXd calculateNonlinearDamping(
+    const geometry_msgs::msg::Twist & velocity) const;
+};
+
+class RestoringForces
+{
+public:
+  RestoringForces(
+    double weight, double buoyancy, const Eigen::Vector3d & center_of_buoyancy,
+    const Eigen::Vector3d & center_of_gravity);
+
+  [[nodiscard]] Eigen::VectorXd calculateRestoringForces(const Eigen::Matrix3d & rotation) const;
 };
 
 }  // namespace blue::dynamics
--- blue_dynamics/test/test_vehicle_dynamics.cpp
+++ blue_dynamics/test/test_vehicle_dynamics.cpp
@@ -25,188 +25,78 @@
 
 #include "blue_dynamics/vehicle_dynamics.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 
 namespace blue::dynamics::test
 {
 
-TEST(VehicleDynamicsTest, CreatesSkewSymmetricMatrix)
+TEST(HydrodynamicsTest, TestInertia)
 {
-  const double a1 = 1.0;
-  const double a2 = 2.0;
-  const double a3 = 3.0;
+  Eigen::Matrix3d moments;
+  moments << 1, 0, 0, 0, 2, 0, 0, 0, 3;
 
-  Eigen::Matrix3d expected;
-  expected << 0, -a3, a2, a3, 0, -a1, -a2, a1, 0;
+  Eigen::VectorXd added_mass_coefficients(6);  // NOLINT
+  added_mass_coefficients << 1, 2, 3, 4, 5, 6;
 
-  const Eigen::Matrix3d actual =
-    blue::dynamics::VehicleDynamics::createSkewSymmetricMatrix(a1, a2, a3);
+  Eigen::MatrixXd added_mass = added_mass_coefficients.asDiagonal().toDenseMatrix();
 
-  ASSERT_TRUE(expected.isApprox(actual));
-}
-
-TEST(VehicleDynamicsTest, CalculatesRigidBodyMassMatrix)
-{
-  const auto moments = blue::dynamics::MomentsOfInertia(1.0, 2.0, 3.0);
   const double mass = 5.0;
 
-  Eigen::VectorXd diagonal(6);  // NOLINT
-  diagonal << mass, mass, mass, moments.x, moments.y, moments.z;
+  const Inertia inertia = Inertia(mass, moments, added_mass);
 
-  const Eigen::MatrixXd expected = diagonal.asDiagonal().toDenseMatrix();
-
-  const Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateRigidBodyMassMatrix(mass, moments);
-
-  ASSERT_TRUE(expected.isApprox(actual));
+  const Eigen::MatrixXd inertia_matrix = inertia.calculateInertia();
 }
 
-TEST(VehicleDynamicsTest, CalculatesAddedMassMatrix)
+TEST(HydrodynamicsTest, TestCoriolis)
 {
-  const blue::dynamics::AddedMass added_mass =
-    blue::dynamics::AddedMass(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+  Eigen::Matrix3d moments;
+  moments << 1, 0, 0, 0, 2, 0, 0, 0, 3;
 
-  const Eigen::MatrixXd expected = -added_mass.toMatrix();
+  Eigen::VectorXd added_mass_coefficients(6);  // NOLINT
+  added_mass_coefficients << 1, 2, 3, 4, 5, 6;
 
-  const Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateAddedMassMatrix(added_mass);
+  Eigen::MatrixXd added_mass = added_mass_coefficients.asDiagonal().toDenseMatrix();
 
-  ASSERT_TRUE(expected.isApprox(actual));
-}
-
-TEST(VehicleDynamicsTest, CalculatesRigidBodyCoriolisMatrix)
-{
-  auto moments = blue::dynamics::MomentsOfInertia(1.0, 2.0, 3.0);
   const double mass = 5.0;
 
-  geometry_msgs::msg::TwistStamped velocity;
-  velocity.twist.angular.x = 1.0;
-  velocity.twist.angular.y = 2.0;
-  velocity.twist.angular.z = 3.0;
+  const Coriolis coriolis = Coriolis(mass, moments, added_mass);
 
-  Eigen::MatrixXd expected(6, 6);
-  expected.topRightCorner(3, 3) = Eigen::MatrixXd::Zero(3, 3);
-  expected.bottomLeftCorner(3, 3) = Eigen::MatrixXd::Zero(3, 3);
-  expected.topLeftCorner(3, 3) =
-    mass * blue::dynamics::VehicleDynamics::createSkewSymmetricMatrix(
-             velocity.twist.angular.x, velocity.twist.angular.y, velocity.twist.angular.z);
+  Eigen::VectorXd velocity(6);  // NOLINT
+  velocity << 1, 2, 3, 4, 5, 6;
 
-  Eigen::Vector3d v2(3);
-  v2 << velocity.twist.angular.x, velocity.twist.angular.y, velocity.twist.angular.z;
-
-  const Eigen::Vector3d moments_v2 = moments.toMatrix() * v2;
-
-  expected.bottomRightCorner(3, 3) = -blue::dynamics::VehicleDynamics::createSkewSymmetricMatrix(
-    moments_v2(0), moments_v2(1), moments_v2(2));
-
-  Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateRigidBodyCoriolisMatrix(mass, moments, velocity);
-
-  ASSERT_TRUE(expected.isApprox(actual));
+  const Eigen::MatrixXd coriolis_matrix = coriolis.calculateCoriolis(velocity);
 }
 
-TEST(VehicleDynamicsTest, CalculatesAddedCoriolisMatrix)
+TEST(HydrodynamicsTest, TestDamping)
 {
-  const blue::dynamics::AddedMass added_mass =
-    blue::dynamics::AddedMass(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+  Eigen::VectorXd linear_damping_coefficients(6);  // NOLINT
+  linear_damping_coefficients << 1, 2, 3, 4, 5, 6;
 
-  geometry_msgs::msg::TwistStamped velocity;
-  velocity.twist.linear.x = 1.0;
-  velocity.twist.linear.y = 2.0;
-  velocity.twist.linear.z = 3.0;
-  velocity.twist.angular.x = 1.0;
-  velocity.twist.angular.y = 2.0;
-  velocity.twist.angular.z = 3.0;
+  Eigen::VectorXd quadratic_damping_coefficients(6);  // NOLINT
+  quadratic_damping_coefficients << 1, 2, 3, 4, 5, 6;
 
-  Eigen::MatrixXd linear = blue::dynamics::VehicleDynamics::createSkewSymmetricMatrix(
-    added_mass.x * velocity.twist.linear.x, added_mass.y * velocity.twist.linear.y,
-    added_mass.z * velocity.twist.linear.z);
+  const Eigen::MatrixXd linear_damping = linear_damping_coefficients.asDiagonal().toDenseMatrix();
 
-  Eigen::MatrixXd expected(6, 6);
-  expected.topLeftCorner(3, 3) = Eigen::MatrixXd::Zero(3, 3);
-  expected.topRightCorner(3, 3) = linear;
-  expected.bottomLeftCorner(3, 3) = linear;
-  expected.bottomRightCorner(3, 3) = blue::dynamics::VehicleDynamics::createSkewSymmetricMatrix(
-    added_mass.k * velocity.twist.angular.x, added_mass.m * velocity.twist.angular.y,
-    added_mass.n * velocity.twist.angular.z);
-
-  Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateAddedCoriolixMatrix(added_mass, velocity);
-
-  ASSERT_TRUE(expected.isApprox(actual));
+  const Eigen::MatrixXd quadratic_damping =
+    quadratic_damping_coefficients.asDiagonal().toDenseMatrix();
 }
 
-TEST(VehicleDynamicsTest, CalculatesLinearDampingMatrix)
+TEST(HydrodynamicsTest, TestRestoringForces)
 {
-  const auto linear = blue::dynamics::LinearDamping(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-  const Eigen::MatrixXd expected = -linear.toMatrix();
-
-  const Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateLinearDampingMatrix(linear);
-
-  ASSERT_TRUE(expected.isApprox(actual));
-}
-
-TEST(VehicleDynamicsTest, CalculatesNonlinearDampingMatrix)
-{
-  geometry_msgs::msg::TwistStamped velocity;
-  velocity.twist.linear.x = -1.0;
-  velocity.twist.linear.y = -2.0;
-  velocity.twist.linear.z = -3.0;
-  velocity.twist.angular.x = -1.0;
-  velocity.twist.angular.y = -2.0;
-  velocity.twist.angular.z = -3.0;
-
-  Eigen::VectorXd velocity_vect(6);  // NOLINT
-  velocity_vect << std::abs(velocity.twist.linear.x), std::abs(velocity.twist.linear.y),
-    std::abs(velocity.twist.linear.z), std::abs(velocity.twist.angular.x),
-    std::abs(velocity.twist.angular.y), std::abs(velocity.twist.angular.z);
-
-  const auto nonlinear = blue::dynamics::NonlinearDamping(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
-  const Eigen::MatrixXd expected =
-    -(nonlinear.toMatrix() * velocity_vect).asDiagonal().toDenseMatrix();
-
-  const Eigen::MatrixXd actual =
-    blue::dynamics::VehicleDynamics::calculateNonlinearDampingMatrix(nonlinear, velocity);
-
-  ASSERT_TRUE(expected.isApprox(actual));
-}
-
-TEST(VehicleDynamicsTest, CalculatesRestoringForcesVector)
-{
-  const double weight = 10.0;
+  const double weight = 5.0;
   const double buoyancy = 7.0;
-  auto pose = geometry_msgs::msg::PoseStamped();
 
-  const auto center_of_gravity = blue::dynamics::CenterOfGravity(0.1, 0.1, 0.3);
+  const Eigen::Vector3d center_of_buoyancy(1, 2, 3);
+  const Eigen::Vector3d center_of_gravity(4, 5, 6);
 
-  // Buoyancy is coincident with the center of gravity
-  const auto center_of_buoyancy = blue::dynamics::CenterOfBuoyancy(0.1, 0.1, 0.3);
+  const RestoringForces restoring_forces =
+    RestoringForces(weight, buoyancy, center_of_buoyancy, center_of_gravity);
 
-  // No rotation
-  pose.pose.orientation.x = 0.0;
-  pose.pose.orientation.y = 0.0;
-  pose.pose.orientation.z = 0.0;
-  pose.pose.orientation.w = 1.0;
+  const Eigen::Quaterniond orientation(1, 0, 0, 0);
+  const Eigen::Matrix3d rot = orientation.toRotationMatrix();
 
-  Eigen::VectorXd expected(6);  // NOLINT
-
-  Eigen::Vector3d fg(0, 0, weight);     // NOLINT
-  Eigen::Vector3d fb(0, 0, -buoyancy);  // NOLINT
-
-  expected.topRows(3) = fg + fb;
-  expected.bottomRows(3) = static_cast<Eigen::Vector3d>(center_of_gravity.toVector()).cross(fg) +
-                           static_cast<Eigen::Vector3d>(center_of_buoyancy.toVector()).cross(fb);
-  expected *= -1;
-
-  const auto vehicle_dynamics = blue::dynamics::VehicleDynamics(
-    0.0, weight, buoyancy, blue::dynamics::MomentsOfInertia(), blue::dynamics::AddedMass(),
-    blue::dynamics::LinearDamping(), blue::dynamics::NonlinearDamping(), center_of_buoyancy,
-    center_of_gravity);
-
-  Eigen::VectorXd actual = vehicle_dynamics.calculateRestoringForcesVector(pose);
-
-  ASSERT_TRUE(expected.isApprox(actual));
+  const Eigen::VectorXd restoring_forces_vec = restoring_forces.calculateRestoringForces(rot);
 }
 
 }  // namespace blue::dynamics::test
