f3854b3c7676a91f4ed4b8295a88e342d5bda5ff
Evan Palmer <evanp922@gmail.com>
2023-05-03 21:01:33 -0700

    Removed unused packages and cleaned up manager implementation

--- blue_manager/blue_manager/manager.py
+++ blue_manager/blue_manager/manager.py
@@ -21,185 +21,101 @@
 from typing import Any
 
 import rclpy
-from mavros_msgs.srv import MessageInterval, ParamGet
-from rclpy.action import ActionServer
+from mavros_msgs.srv import MessageInterval, ParamGet, ParamSetV2
 from rclpy.node import Node
 from sensor_msgs.msg import BatteryState
-
-from blue_msgs.action import Arm, Connect
+from std_srvs.srv import SetBool
 
 
 class Manager(Node):
+    STOPPED_PWM = 1500
+
     def __init__(self) -> None:
         super().__init__("blue_manager")
 
+        self.battery_state = BatteryState()
         self.connected = False
         self.armed = False
-        self.backup_settings: dict[str, Any] = {}
-        self.battery_charge = 0.0
-
-        # Declare the manager parameters
-        self.declare_parameters(
-            "",
-            [
-                ("min_battery_percent", 15.0),
-                ("stopped_pwm", 1500),
-                ("rov_config", "heavy"),
-                ("max_connection_attempts", 3),
-                ("connection_attempt_timeout", 1.0),
-            ],
-        )
-
-        # Load the parameter values
-        self.total_battery_cells = (
-            self.get_parameter("total_battery_cells")
-            .get_parameter_value()
-            .integer_value
-        )
-        self.min_battery_percent = (
-            self.get_parameter("min_battery_percent").get_parameter_value().double_value
-        )
-        self.rov_config = (
-            self.get_parameter("rov_config").get_parameter_value().string_value
-        )
-        self.max_connection_attempts = (
-            self.get_parameter("max_connection_attempts")
-            .get_parameter_value()
-            .integer_value
-        )
-        self.connection_attempt_timeout = (
-            self.get_parameter("connection_attempt_timeout")
-            .get_parameter_value()
-            .double_value
-        )
-        self.stopped_pwm = (
-            self.get_parameter("stopped_pwm").get_parameter_value().integer_value
-        )
-
-        # Service clients
-        self.set_message_interval_client = self.create_client(
-            MessageInterval, "/mavros/set_message_interval", 1
-        )
-
-        # Action servers
-        self.connect_action_server = ActionServer(
-            self, Connect, "/blue/connect", self._connect_control_manager_cb
-        )
-        self.arm_action_server = ActionServer(
-            self, Arm, "/blue/arm", self._arm_controller_cb
-        )
+        self.rov_config = "heavy"
+        self.backup_params: dict[str, Any] = {}
 
         # Subscribers
         self.battery_sub = self.create_subscription(
-            BatteryState, "/mavros/battery", self._update_battery_level_cb
+            BatteryState, "/mavros/battery", self._store_battery_state_cb
         )
 
-    def _connect_control_manager_cb(
-        self, connection_request: Connect.Goal
-    ) -> Connect.Result:
-        connection_result = Connect.Result()
+        # Services
+        self.arm_srv = self.create_service(
+            SetBool, "/blue/connect", self._manage_connection_cb
+        )
+        self.connect_srv = self.create_service(
+            SetBool, "/blue/arm", self._manage_arming_cb
+        )
 
-        if connection_request.connect:
-            result = self.connect(
-                self.max_connection_attempts, self.connection_attempt_timeout
+        # Service clients
+        self.set_message_rate_srv_client = self.create_client(
+            MessageInterval, "/mavros/set_message_interval", 1
+        )
+        self.get_param_srv_client = self.create_client(ParamGet, "/mavros/param/get", 1)
+        self.set_param_srv_client = self.create_client(
+            ParamSetV2, "/mavros/param/set", 1
+        )
+
+    def _store_battery_state_cb(self, state: BatteryState) -> None:
+        self.battery_state = state
+
+    def check_battery_state(self) -> bool:
+        return self.battery_state.percentage <= 0.2
+
+    def _manage_connection_cb(self, connect: bool) -> None:
+        if connect:
+            self.get_logger().info(
+                "Attempting to establish a connection with the BlueROV2..."
             )
+
+            self.connected = self.connect()
+
+            if not self.connected:
+                self.get_logger().warning(
+                    "Failed to establish a connection with the BlueROV2."
+                )
+            else:
+                self.get_logger().info(
+                    "Successfully established a connection with the BlueROV2."
+                )
         else:
-            result = self.disconnect(
-                self.max_connection_attempts, self.connection_attempt_timeout
+            self.get_logger().info(
+                "Attempting to shutdown the connection to the BlueROV2..."
             )
+            self.connected = not self.disconnect()
 
-        connection_result.success = result
+            if not self.connected:
+                self.get_logger().warning(
+                    "Successfully shutdown the connection to the BlueROV2."
+                )
+            else:
+                self.get_logger().info("Failed to perform all shutdown proceedures.")
 
-        return connection_result
-
-    def _arm_controller_cb(self, arm_request: Arm.Goal) -> Arm.Result:
-        arm_result = Connect.Result()
-
-        if arm_request.arm:
-            result = self.arm()
-        else:
-            result = self.disarm()
-
-        arm_result.success = result
-
-        return arm_result
-
-    def set_message_intervals(self) -> None:
+    def connect(self) -> bool:
         ...
 
-    def connect(self, max_attempts: int, timeout: float) -> bool:
-        for _ in range(max_attempts):
-            self.connected = self.backup_motor_params(timeout)
+    def backup_motor_params(self) -> bool:
+        ...
 
-            if self.connected:
-                break
+    def disconnect(self) -> bool:
+        ...
 
-        return self.connected
+    def restore_motor_param_backup(self) -> bool:
+        ...
 
-    def disconnect(self, max_attempts: int, timeout: float) -> bool:
-        disarmed = False
-        backup_restored = False
-
-        for _ in range(max_attempts):
-            if not disarmed:
-                disarmed = self.disarm()
-
-            if not backup_restored:
-                backup_restored = self.restore_param_backup(timeout)
-
-            if disarmed and backup_restored:
-                break
-
-        success = disarmed and backup_restored
-        self.connected = not success
-
-        return success
+    def _manage_arming_cb(self, arm: bool) -> None:
+        ...
 
     def arm(self) -> bool:
-        if not self.check_battery_level():
-            self.get_logger().warning(
-                f"Failed to arm the system: the current battery level ("
-                f" {self.battery_charge}%) is below the minimum battery level."
-            )
-            return False
-
-        self.armed = self.enable_pwm_passthrough()
-
-        return self.armed
+        ...
 
     def disarm(self) -> bool:
-        success = self.stop_thrusters() and self.disable_pwm_passthrough()
-        self.armed = not success
-        return success
-
-    def backup_motor_params(self, timeout: float) -> bool:
         ...
-
-    def restore_param_backup(self, timeout: float) -> bool:
-        ...
-
-    def stop_thrusters(self) -> bool:
-        ...
-
-    def enable_pwm_passthrough(self) -> bool:
-        ...
-
-    def disable_pwm_passthrough(self) -> bool:
-        ...
-
-    def check_battery_level(self) -> bool:
-        return self.battery_charge < self.min_battery_percent
-
-    def _update_battery_level_cb(self, battery_state: BatteryState) -> float:
-        self.battery_charge = battery_state.percentage
-
-        if not self.check_battery_level():
-            self.get_logger().warning(
-                f"The current battery level ({self.battery_charge * 100}%) is below"
-                f" the minimum charge level. Disarming the system."
-            )
-
-            self.disarm()
 
 
 def main(args: list[str] | None = None):
