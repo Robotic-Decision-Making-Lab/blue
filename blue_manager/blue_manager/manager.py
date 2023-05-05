4f1230ce18c059dacdc5960b74c3e13e82a73353
Evan Palmer <evanp922@gmail.com>
2023-05-05 00:53:01 -0700

    Implemented and tested parameter backup procedure, started on passthrough mode enabling

--- blue_manager/blue_manager/manager.py
+++ blue_manager/blue_manager/manager.py
@@ -18,103 +18,238 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from typing import Any
+from copy import deepcopy
+from functools import partial
 
 import rclpy
-from mavros_msgs.srv import MessageInterval, ParamGet, ParamSetV2
+from mavros_msgs.msg import ParamEvent
+from rcl_interfaces.msg import Parameter, ParameterType
+from rcl_interfaces.srv import GetParameters, SetParameters
 from rclpy.node import Node
-from sensor_msgs.msg import BatteryState
+from rclpy.task import Future
 from std_srvs.srv import SetBool
 
 
 class Manager(Node):
+    """Provides custom controllers with an interface to the BlueROV2 thrusters."""
+
     STOPPED_PWM = 1500
 
     def __init__(self) -> None:
+        """Create a new control manager."""
         super().__init__("blue_manager")
 
-        self.battery_state = BatteryState()
         self.connected = False
         self.armed = False
-        self.rov_config = "heavy"
-        self.backup_params: dict[str, Any] = {}
+
+        self.declare_parameters("", [("rov_config", "heavy")])
+
+        rov_config = self.get_parameter("rov_config").get_parameter_value().string_value
+
+        # The BlueROV2 has two configurations: the heavy configuration with 8 thrusters
+        # and the standard configuration with 6 thrusters.
+        match rov_config:
+            case "heavy":
+                num_thrusters = 8
+            case _:
+                num_thrusters = 6
+
+        # Maintain a backup of the thruster parameters so that we can restore them when
+        # disconnecting
+        self.thruster_params_backup: dict[str, Parameter | None] = {
+            f"SERVO{i}_FUNCTION": None for i in range(1, num_thrusters + 1)
+        }
 
         # Subscribers
-        self.battery_sub = self.create_subscription(
-            BatteryState, "/mavros/battery", self._store_battery_state_cb
+        self.param_event_sub = self.create_subscription(
+            ParamEvent, "/mavros/param/event", self.save_default_thruster_params_cb, 10
         )
 
         # Services
-        self.arm_srv = self.create_service(
-            SetBool, "/blue/connect", self._manage_connection_cb
-        )
         self.connect_srv = self.create_service(
-            SetBool, "/blue/arm", self._manage_arming_cb
+            SetBool, "/blue/connect", self.manage_connection_cb
         )
+
+        def wait_for_service(client):
+            while not client.wait_for_service(timeout_sec=1.0):
+                ...
 
         # Service clients
-        self.set_message_rate_srv_client = self.create_client(
-            MessageInterval, "/mavros/set_message_interval", 1
+        self.get_param_srv_client = self.create_client(
+            GetParameters, "/mavros/param/get_parameters"
         )
-        self.get_param_srv_client = self.create_client(ParamGet, "/mavros/param/get", 1)
+        wait_for_service(self.get_param_srv_client)
+
         self.set_param_srv_client = self.create_client(
-            ParamSetV2, "/mavros/param/set", 1
+            SetParameters, "/mavros/param/set_parameters"
+        )
+        wait_for_service(self.set_param_srv_client)
+
+    @property
+    def thruster_params_backed_up(self) -> bool:
+        """All required thruster parameters have been successfully backed up.
+
+        Returns:
+            Whether or not the thruster parameters have been backed up.
+        """
+        return None not in self.thruster_params_backup.values()
+
+    def save_default_thruster_params_cb(self, event: ParamEvent) -> None:
+        """Save the default thruster parameter values to the parameter backup.
+
+        MAVROS publishes the parameter values of all ArduSub parameters when
+        populating the parameter server. We subscribe to this topic to receive the
+        messages at the same time as MAVROS so that we can avoid duplicating requests
+        to the FCU.
+
+        Args:
+            event: The default parameter loading event triggered by MAVROS.
+        """
+        if (
+            event.param_id in self.thruster_params_backup
+            and self.thruster_params_backup[event.param_id] is None
+        ):
+            self.thruster_params_backup[event.param_id] = Parameter(
+                name=event.param_id, value=event.value
+            )
+
+            if self.thruster_params_backed_up:
+                self.get_logger().info(
+                    "Successfully backed up the ArduSub thruster parameters."
+                )
+
+    def request_ardusub_params(self, param_ids: list[str]) -> Future:
+        """Request parameter values from ArduSub.
+
+        Args:
+            param_ids: The names of the parameters whose values should be requested.
+
+        Returns:
+            A future object which provides access to the parameter request result.
+        """
+        return self.get_param_srv_client.call_async(
+            GetParameters.Request(names=param_ids)
         )
 
-    def _store_battery_state_cb(self, state: BatteryState) -> None:
-        self.battery_state = state
+    def request_thruster_params(self, thruster_param_ids: list[str]) -> None:
+        """Request the thruster parameters which will be overwritten.
 
-    def check_battery_state(self) -> bool:
-        return self.battery_state.percentage <= 0.2
+        The thruster parameters requested include all MOTORn_FUNCTION parameters
+        assigned to be motors. These are modified by the manager to support the PWM
+        Pass-through mode, but need to be restored when the manager is shutdown.
 
-    def _manage_connection_cb(self, connect: bool) -> None:
-        if connect:
-            self.get_logger().info(
-                "Attempting to establish a connection with the BlueROV2..."
-            )
+        Args:
+            thruster_param_ids: The names of the thruster parameters whose values
+                should be backed up.
+        """
 
-            self.connected = self.connect()
-
-            if not self.connected:
-                self.get_logger().warning(
-                    "Failed to establish a connection with the BlueROV2."
-                )
+        def handle_request_future_cb(future: Future, param_ids: list[str]):
+            try:
+                response = future.result()
+            except Exception:
+                ...
             else:
-                self.get_logger().info(
-                    "Successfully established a connection with the BlueROV2."
-                )
+                # The parameter values are provided in the same order as the names
+                # were provided in
+                for param_value, param_id in zip(response.values, param_ids):
+                    self.thruster_params_backup[param_id] = Parameter(
+                        name=param_id, value=param_value
+                    )
+
+        future = self.request_ardusub_params(thruster_param_ids)
+        future.add_done_callback(
+            partial(handle_request_future_cb, param_ids=thruster_param_ids)
+        )
+
+    def manage_connection_cb(
+        self, request: SetBool.Request, response: SetBool.Response
+    ) -> SetBool.Response:
+        """Connect/disconnect the manager.
+
+        Args:
+            request: The connection service request.
+            response: The connection service response.
+
+        Returns:
+            The connection service response.
+        """
+        if request.data:
+            response.success = self.connect()
         else:
-            self.get_logger().info(
-                "Attempting to shutdown the connection to the BlueROV2..."
-            )
-            self.connected = not self.disconnect()
+            response.success = True
 
-            if not self.connected:
-                self.get_logger().warning(
-                    "Successfully shutdown the connection to the BlueROV2."
-                )
-            else:
-                self.get_logger().info("Failed to perform all shutdown proceedures.")
+        # TODO(Evan): This is wrong
+        self.connected = response.success
+
+        return response
 
     def connect(self) -> bool:
-        ...
+        self.get_logger().warning(
+            "Attempting to connect the manager to the BlueROV2. All ArduSub arming"
+            " and failsafe procedures will be disabled upon successful connection."
+        )
+        if not self.thruster_params_backed_up:
+            # Get the IDs of the parameters that weren't successfully backed up on
+            # initialization
+            unsaved_params = [
+                param_id
+                for param_id in self.thruster_params_backup.keys()
+                if self.thruster_params_backup[param_id] is None
+            ]
 
-    def backup_motor_params(self) -> bool:
-        ...
+            self.request_thruster_params(unsaved_params)
+
+            if not self.thruster_params_backed_up:
+                self.get_logger().error(
+                    "Connection attempt failed. The system has not finished backing"
+                    " up the thruster parameters or the backup process failed."
+                )
+                return False
+
+        # Set the motors to PWM passthrough mode
+        return self.set_pwm_passthrough_mode()
+
+    def set_pwm_passthrough_mode(self) -> bool:
+        if not self.thruster_params_backed_up:
+            self.get_logger().error(
+                "The thrusters cannot be set to PWM Pass-through mode without first"
+                " being backed up."
+            )
+            return False
+
+        passthrough_params = deepcopy(self.thruster_params_backup)
+
+        # Set the servo mode to "DISABLED"
+        # This disables the arming and failsafe features, but now lets us send PWM
+        # values to the thrusters without any mixing
+        for param in passthrough_params.values():
+            param.value.integer_value = 0  # type: ignore
+            param.value.type = ParameterType.PARAMETER_INTEGER  # type: ignore
+
+        # We would actually prefer to set all of the parameters atomically; however,
+        # this functionality is not supported by MAVROS
+        set_param_request = SetParameters.Request(
+            parameters=list(passthrough_params.values())
+        )
+
+        self.set_param_srv_client.call_async(set_param_request).add_done_callback(
+            self.this_is_a_test
+        )
+
+        return True
+
+    def this_is_a_test(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().info("Service call failed %r" % (e,))
+        else:
+            self.get_logger().info(f"Parameter set succeed..maybe?: {response}")
 
     def disconnect(self) -> bool:
-        ...
+        return False
 
     def restore_motor_param_backup(self) -> bool:
-        ...
-
-    def _manage_arming_cb(self, arm: bool) -> None:
-        ...
-
-    def arm(self) -> bool:
-        ...
-
-    def disarm(self) -> bool:
         ...
 
 
--- blue_manager/setup.py
+++ blue_manager/setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup
 
-package_name = 'blue_manager'
+package_name = "blue_manager"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Evan Palmer',
-    maintainer_email='evanp922@gmail.com',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Evan Palmer",
+    maintainer_email="evanp922@gmail.com",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "blue_manager = blue_manager.manager:main",
         ],
     },
 )
