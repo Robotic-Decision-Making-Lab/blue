bbf6c8302c2cb831eac617f9d98909b72d785274
Evan Palmer <evanp922@gmail.com>
2023-05-09 14:25:56 -0700

    Added docstrings

--- blue_dynamics/include/blue_dynamics/thruster_dynamics.hpp
+++ blue_dynamics/include/blue_dynamics/thruster_dynamics.hpp
@@ -25,7 +25,27 @@
 namespace blue::dynamics
 {
 
+/**
+ * @brief Calculates the T200 deadzone band for a given voltage.
+ *
+ * @param voltage The current battery voltage.
+ * @return The minimum and maximum PWM values for the deadzone band.
+ */
 [[nodiscard]] std::tuple<int, int> calculateDeadZone(double voltage);
+
+/**
+ * @brief Approximates the PWM input needed to achieve a desired force given the current battery
+ * voltage.
+ *
+ * @note This is a simplified technique for calculating the PWM input. A more accurate method would
+ * perform a characterization of the thruster dynamics. The issue with that solution, however, is
+ * that there is no feedback from the BlueROV2 thrusters to use as the state variables for the
+ * system.
+ *
+ * @param force The desired force (N).
+ * @param voltage The current battery voltage (V).
+ * @return The PWM input needed to achieve the desired force.
+ */
 [[nodiscard]] int calculatePwmFromThrustSurface(double force, double voltage);
 
 }  // namespace blue::dynamics
