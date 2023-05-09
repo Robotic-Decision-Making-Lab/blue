adc56e5042372d0d4ca5287194c018c88daaf839
Evan Palmer <evanp922@gmail.com>
2023-05-08 19:35:14 -0700

    Finalized initial version

--- blue_manager/blue_manager/manager.py
+++ blue_manager/blue_manager/manager.py
@@ -21,8 +21,7 @@
 from copy import deepcopy
 
 import rclpy
-from mavros_msgs.msg import ParamEvent
-from mavros_msgs.srv import CommandLong
+from mavros_msgs.msg import OverrideRCIn, ParamEvent
 from rcl_interfaces.msg import Parameter
 from rcl_interfaces.srv import SetParameters
 from rclpy.callback_groups import ReentrantCallbackGroup
@@ -40,20 +39,61 @@ class Manager(Node):
         """Create a new control manager."""
         super().__init__("blue_manager")
 
+        self.passthrough_enabled = False
+        self.num_thrusters = 8
+        self.timeout = 1.0
+        self.retries = 3
+
+        # Get the ROS parameters from the parameter server
+        self._get_ros_params()
+
         # We need a reentrant callback group to get synchronous calls to services from
-        # within services
+        # within service callbacks.
         reentrant_callback_group = ReentrantCallbackGroup()
 
+        self.thruster_params_backup: dict[str, Parameter | None] = {
+            f"SERVO{i}_FUNCTION": None for i in range(1, self.num_thrusters + 1)
+        }
+
+        # Publishers
+        self.override_rc_in_pub = self.create_publisher(
+            OverrideRCIn, "/mavros/rc/override", 1
+        )
+
+        # Subscribers
+        self.param_event_sub = self.create_subscription(
+            ParamEvent, "/mavros/param/event", self.backup_thruster_params_cb, 50
+        )
+
+        # Services
+        self.set_pwm_passthrough_srv = self.create_service(
+            SetBool,
+            "/blue/manager/enable_passthrough",
+            self.set_rc_passthrough_mode_cb,
+            callback_group=reentrant_callback_group,
+        )
+
+        # Service clients
+        self.set_param_srv_client = self.create_client(
+            SetParameters,
+            "/mavros/param/set_parameters",
+            callback_group=reentrant_callback_group,
+        )
+
+        while not self.set_param_srv_client.wait_for_service(timeout_sec=1.0):
+            ...
+
+    def _get_ros_params(self) -> None:
+        """Get the ROS parameters from the parameter server."""
         self.declare_parameters(
             "",
             [
-                ("num_thrusters", 8),
-                ("mode_change_timeout", 1.0),
-                ("mode_change_retries", 3),
+                ("num_thrusters", self.num_thrusters),
+                ("mode_change_timeout", self.timeout),
+                ("mode_change_retries", self.retries),
             ],
         )
 
-        self.passthrough_enabled = False
         self.num_thrusters = (
             self.get_parameter("num_thrusters").get_parameter_value().integer_value
         )
@@ -65,39 +105,6 @@ class Manager(Node):
             .get_parameter_value()
             .integer_value
         )
-        self.thruster_params_backup: dict[str, Parameter | None] = {
-            f"SERVO{i}_FUNCTION": None for i in range(1, self.num_thrusters + 1)
-        }
-
-        # Subscribers
-        self.param_event_sub = self.create_subscription(
-            ParamEvent, "/mavros/param/event", self.backup_thruster_params_cb, 50
-        )
-
-        # Services
-        self.set_pwm_passthrough_srv = self.create_service(
-            SetBool,
-            "/blue/manager/enable_passthrough",
-            self.set_pwm_passthrough_mode_cb,
-            callback_group=reentrant_callback_group,
-        )
-
-        def wait_for_service(client, timeout=1.0) -> None:
-            while not client.wait_for_service(timeout_sec=timeout):
-                self.get_logger().warning("Waiting for service to become available.")
-
-        # Service clients
-        self.set_param_srv_client = self.create_client(
-            SetParameters,
-            "/mavros/param/set_parameters",
-            callback_group=reentrant_callback_group,
-        )
-        wait_for_service(self.set_param_srv_client)
-
-        # self.command_long_srv_client = self.create_client(
-        #     CommandLong, "/mavros/cmd/long", callback_group=reentrant_callback_group
-        # )
-        # wait_for_service(self.command_long_srv_client)
 
     def backup_thruster_params_cb(self, event: ParamEvent) -> None:
         """Backup the default thruster parameter values.
@@ -123,34 +130,36 @@ class Manager(Node):
                     "Successfully backed up the thruster parameters."
                 )
 
-    def set_pwm(self, thruster: int, pwm: int) -> bool:
-        """Set the PWM value of a thruster.
+    def set_rc(self, pwm: list[int]) -> None:
+        """Set the PWM values of the thrusters.
 
         Args:
-            thruster: The thruster number to set the PWM value of.
-            pwm: The PWM value to set the thruster to.
+            pwm: The PWM values to set the thruster to. The length of the provided list
+                must be equal to the number of thrusters.
 
         Returns:
             The result of the PWM setting call.
         """
-        result: CommandLong.Response = self.command_long_srv_client.call(
-            CommandLong(command=183, confirmation=0, param1=thruster, param2=pwm)
-        )
-        return result.success
+        if len(pwm) != self.num_thrusters:
+            raise ValueError(
+                "The length of the PWM input must equal the number of thrusters."
+            )
+
+        # Change the values of only the thruster channels
+        channels = pwm + [OverrideRCIn.CHAN_NOCHANGE] * (18 - self.num_thrusters)
+
+        self.override_rc_in_pub.publish(OverrideRCIn(channels=channels))
 
     def stop_thrusters(self) -> None:
         """Stop all thrusters."""
-        self.get_logger().warning("Attempting to stop all BlueROV2 thrusters.")
-        for i in range(1, self.num_thrusters + 1):
-            for _ in range(self.retries):
-                if self.set_pwm(i, self.STOPPED_PWM):
-                    break
+        self.get_logger().warning("Stopping all BlueROV2 thrusters.")
+        self.set_rc([self.STOPPED_PWM] * self.num_thrusters)
 
-    def set_thruster_params(self, parameters: list[Parameter]) -> bool:
+    def set_thruster_params(self, params: list[Parameter]) -> bool:
         """Set the thruster parameters.
 
         Args:
-            parameters: The parameters to set.
+            params: The parameters to set.
 
         Returns:
             True if the parameters were successfully set, False otherwise.
@@ -158,17 +167,22 @@ class Manager(Node):
         # We would actually prefer to set all of the parameters atomically, but
         # this functionality is not currently supported by MAVROS
         response: SetParameters.Response = self.set_param_srv_client.call(
-            SetParameters.Request(parameters=parameters)
+            SetParameters.Request(parameters=params)
         )
         return all([result.successful for result in response.results])
 
-    def set_pwm_passthrough_mode_cb(
+    def set_rc_passthrough_mode_cb(
         self, request: SetBool.Request, response: SetBool.Response
     ) -> SetBool.Response:
-        """Set the PWM Passthrough mode.
+        """Set the RC Passthrough mode.
+
+        RC Passthrough mode enables users to control the BlueROV2 thrusters directly
+        using the RC channels. It is important that users disable their RC transmitter
+        prior to enabling RC Passthrough mode to avoid sending conflicting commands to
+        the thrusters.
 
         Args:
-            request: The request to enable/disable PWM passthrough mode.
+            request: The request to enable/disable RC passthrough mode.
             response: The result of the request.
 
         Returns:
@@ -177,31 +191,29 @@ class Manager(Node):
         if request.data:
             if self.passthrough_enabled:
                 response.success = True
-                response.message = "The system is already in PWM Passthrough mode."
+                response.message = "The system is already in RC Passthrough mode."
                 return response
 
             if None in self.thruster_params_backup.values():
                 response.success = False
                 response.message = (
-                    "The thrusters cannot be set to PWM Passthrough mode without first"
+                    "The thrusters cannot be set to RC Passthrough mode without first"
                     " being successfully backed up."
                 )
                 return response
 
-            # self.stop_thrusters()
-
             self.get_logger().warning(
-                "Attempting to switch to the PWM Passthrough flight mode. All ArduSub"
+                "Attempting to switch to the RC Passthrough flight mode. All ArduSub"
                 " arming and failsafe procedures will be disabled upon success."
             )
 
             passthrough_params = deepcopy(self.thruster_params_backup)
 
-            # Set the servo mode to "DISABLED"
+            # Set the servo mode to "RC Passthrough"
             # This disables the arming and failsafe features, but now lets us send PWM
             # values to the thrusters without any mixing
             for param in passthrough_params.values():
-                param.value.integer_value = 0  # type: ignore
+                param.value.integer_value = 1  # type: ignore
 
             for _ in range(self.retries):
                 self.passthrough_enabled = self.set_thruster_params(
@@ -213,20 +225,22 @@ class Manager(Node):
                     break
 
             if response.success:
-                response.message = "Successfully switched to PWM Passthrough mode."
+                response.message = "Successfully switched to RC Passthrough mode."
+
+                self.stop_thrusters()
             else:
-                response.message = "Failed to switch to PWM Passthrough mode."
+                response.message = "Failed to switch to RC Passthrough mode."
         else:
             if not self.passthrough_enabled:
                 response.success = True
                 response.message = (
-                    "The system was not in the PWM Passthrough mode to start with."
+                    "The system was not in the RC Passthrough mode to start with."
                 )
                 return response
 
-            # self.stop_thrusters()
+            self.stop_thrusters()
 
-            self.get_logger().warning("Attempting to disable PWM Passthrough mode.")
+            self.get_logger().warning("Attempting to disable RC Passthrough mode.")
 
             for _ in range(self.retries):
                 self.passthrough_enabled = not self.set_thruster_params(
@@ -238,16 +252,18 @@ class Manager(Node):
                     break
 
             if response.success:
-                response.message = "Successfully left PWM Passthrough mode."
+                response.message = "Successfully left RC Passthrough mode."
             else:
                 self.get_logger().warning(
-                    "Failed to leave the PWM Passthrough mode. If failure persists,"
+                    "Failed to leave the RC Passthrough mode. If failure persists,"
                     " the following backup parameters may be restored manually using"
                     f" QGC: {self.thruster_params_backup}"
                 )
                 response.message = (
-                    "Failed to leave PWM Passthrough mode. Good luck soldier."
+                    "Failed to leave RC Passthrough mode. Good luck soldier."
                 )
+
+        self.get_logger().info(response.message)
 
         return response
 
