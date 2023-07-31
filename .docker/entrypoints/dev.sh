084c1acb45fd020d96d14c34250b7c8f7a75e234
Evan Palmer <evanp922@gmail.com>
2023-07-30 18:39:48 -0700

    almost broke entrypoint again

--- .docker/entrypoints/dev.sh
+++ .docker/entrypoints/dev.sh
@@ -4,4 +4,4 @@
 export GZ_SIM_RESOURCE_PATH=/workspaces/blue/blue_description/gazebo/models:/workspaces/blue/blue_description/gazebo/worlds:$GZ_SIM_RESOURCE_PATH
 
 # Add blue meshes
-export GZ_SIM_RESOURCE_PATH=/workspaces/blue/blue_description/meshes
+export GZ_SIM_RESOURCE_PATH=/workspaces/blue/blue_description/meshes:$GZ_SIM_RESOURCE_PATH
