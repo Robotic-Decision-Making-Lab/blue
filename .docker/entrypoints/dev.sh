2c4e2490b5cda4fddafdf6d028f8a8e96a91f263
Evan Palmer <evanp922@gmail.com>
2023-10-12 17:49:15 -0500

    Cleaned up Docker pipelines (#121)
    * Cleaned up Dockerfile and added new user entrypoints
    
    * Fixed robot pipeline to support building for arm64
    
    * Updated devcontainer configurations to use latest changes
    
    * Testing pipelines
    
    * Removed testing branch from pipelines
    
    * Precommit
    
    * Resolved pr comments
    
    (cherry picked from commit 11807c8e7e5a19faf4b136d9b8c35458338a0fe9)
    
    # Conflicts:
    #	.devcontainer/nouveau/devcontainer.json
    #	.devcontainer/nvidia/devcontainer.json
    #	.github/ISSUE_TEMPLATE/config.yaml
    #	.github/workflows/ci.yaml

--- .docker/entrypoints/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Add blue models and worlds
-export GZ_SIM_RESOURCE_PATH=/workspaces/blue/blue_description/gazebo/models:/workspaces/blue/blue_description/gazebo/worlds:$GZ_SIM_RESOURCE_PATH
-
-# Add blue meshes
-export GZ_SIM_RESOURCE_PATH=/workspaces/blue/blue_description/meshes:$GZ_SIM_RESOURCE_PATH
